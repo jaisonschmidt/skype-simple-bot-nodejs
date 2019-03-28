d29067e2695007d24977dfe585692234cea721a9
Jaison Schmidt <jaison.schmidt@compasso.com.br>
2019-03-28 15:46:47 -0300

    Ajuste

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var calaboca = false;
 var server = restify.createServer();
 
 server.listen(process.env.port || process.env.PORT || 8080, function () {
-   console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
@@ -33,8 +33,8 @@ bot.on('contactRelationUpdate', function (message) {
     if (message.action === 'add') {
         var name = message.user ? message.user.name : null;
         var reply = new builder.Message()
-                .address(message.address)
-                .text("Oi %s... Obrigado por me adicionar!", name || 'pessoa legal');
+            .address(message.address)
+            .text("Oi %s... Obrigado por me adicionar!", name || 'pessoa legal');
         bot.send(reply);
     } else {
         // delete their data
@@ -42,7 +42,7 @@ bot.on('contactRelationUpdate', function (message) {
 });
 
 bot.on('typing', function (message) {
-  // User is typing
+    // User is typing
 });
 
 bot.on('deleteUserData', function (message) {
@@ -52,75 +52,79 @@ bot.on('deleteUserData', function (message) {
 //=========================================================
 // Bots Dialogs
 //=========================================================
-String.prototype.contains = function(content){
-  return this.indexOf(content) !== -1;
+String.prototype.contains = function (content) {
+    return this.indexOf(content) !== -1;
 }
 
 bot.dialog('/', function (session) {
-    
+
     //users.push({ name : session.message.user.name, address : session.message.address });
 
-    if(session.message.user.name=="Jaison Schmidt" && session.message.text.toLowerCase().contains('calaboca')){
+    if (session.message.user.name == "Jaison Schmidt" && session.message.text.toLowerCase().contains('calaboca')) {
         session.send(`Ta bom!`);
         calaboca = true;
         return true;
     }
 
-    if(session.message.user.name=="Jaison Schmidt" && session.message.text.toLowerCase().contains('melhor amigo')){
+    if (session.message.user.name == "Jaison Schmidt" && session.message.text.toLowerCase().contains('melhor amigo')) {
         session.send(`O Jonaaaaaas! :D`);
         return true;
     }
 
-    if(session.message.user.name=="Jaison Schmidt" && session.message.text.toLowerCase().contains('pode falar')){
+    if (session.message.user.name == "Jaison Schmidt" && session.message.text.toLowerCase().contains('pode falar')) {
         session.send(`Heeeeeee`);
         calaboca = false;
         return true;
     }
 
-    if(calaboca==true && session.message.user.name!="Jaison Schmidt"){
+    if (calaboca == true && session.message.user.name != "Jaison Schmidt") {
         return true;
     }
 
-    if(session.message.text.toLowerCase().contains('meme ue')){
+    if (session.message.text.toLowerCase().contains('meme ue')) {
         session.send(`https://forum.gc.historygames.net/uploads/monthly_2016_07/64510606.jpg.71550efcca9d919f6bde8b2dcde4beeb.jpg`);
-    } else if(session.message.text.toLowerCase().contains('meme is')){
+    } else if (session.message.text.toLowerCase().contains('meme is')) {
         session.send(`https://rlv.zcache.com/its_something_meme_postcard-r048747b5c6b24a2381258f05fd149b78_vgbaq_8byvr_324.jpg`);
-    } else if(session.message.text.toLowerCase().contains('manda nudes')){
+    } else if (session.message.text.toLowerCase().contains('manda nudes')) {
         session.send(`https://img.ibxk.com.br/2015/06/15/15180943425877.png?w=1040`);
-    } else if(session.message.text.toLowerCase().contains('tchau')){
+    } else if (session.message.text.toLowerCase().contains('tchau')) {
         session.send(`Tchau ${session.message.user.name}! :D`);
-    } 
-    
-    else if(session.message.text.toLowerCase().contains('bom dia!')){
+    }
+
+    else if (session.message.text.toLowerCase().contains('bom dia!')) {
         session.send(`Bom dia ${session.message.user.name}! :D`);
-    } 
+    }
 
-    else if(session.message.text.toLowerCase().contains('da bom dia')){
+    else if (session.message.text.toLowerCase().contains('seu cargo')) {
+        session.send(`sênior! :-)`);
+    }
+
+    else if (session.message.text.toLowerCase().contains('da bom dia')) {
         var lastWord = session.message.text.split(" ");
-        session.send(`Bom dia ${lastWord[lastWord.length-1]}! :D`);
-    } 
+        session.send(`Bom dia ${lastWord[lastWord.length - 1]}! :D`);
+    }
 
-    else if(session.message.text.toLowerCase().contains('boa tarde!')){
+    else if (session.message.text.toLowerCase().contains('boa tarde!')) {
         session.send(`Boa tarde ${session.message.user.name}! :D`);
-    } 
-    else if(session.message.text.toLowerCase().contains('da boa tarde')){
+    }
+    else if (session.message.text.toLowerCase().contains('da boa tarde')) {
         var lastWord = session.message.text.split(" ");
-        session.send(`Boa tarde ${lastWord[lastWord.length-1]}! :D`);
-    }  else if(session.message.text.toLowerCase().contains('quina')){
+        session.send(`Boa tarde ${lastWord[lastWord.length - 1]}! :D`);
+    } else if (session.message.text.toLowerCase().contains('quina')) {
         var arr = []
-    
-        while(arr.length < 5){
-            var randomnumber = Math.ceil(Math.random()*80)
-            if(arr.indexOf(randomnumber) > -1) continue;
+
+        while (arr.length < 5) {
+            var randomnumber = Math.ceil(Math.random() * 80)
+            if (arr.indexOf(randomnumber) > -1) continue;
             arr[arr.length] = randomnumber;
         }
-        
-        session.send(arr.sort(function(a, b){return a-b}).toString());
 
-    } else if (session.message.text.toLowerCase().contains('previsao do tempo')){
+        session.send(arr.sort(function (a, b) { return a - b }).toString());
+
+    } else if (session.message.text.toLowerCase().contains('previsao do tempo')) {
 
         var url = "https://api.hgbrasil.com/weather/?format=json&woeid=455896"
-        
+
         request({
             url: url,
             json: true
@@ -129,35 +133,35 @@ bot.dialog('/', function (session) {
             session.send(`Em ${amanha.date} a previsao é de ${amanha.description}. Minima de ${amanha.min} e maxima de ${amanha.max}.`);
         })
 
-    } 
-    else if(session.message.text.toLowerCase().contains('valeu man!')){
+    }
+    else if (session.message.text.toLowerCase().contains('valeu man!')) {
         session.send(`de nada man, tmj!`);
-    } 
-    
-    else if(session.message.text.toLowerCase().contains('piada')){
+    }
+
+    else if (session.message.text.toLowerCase().contains('piada')) {
         // nao funciona
         session.send("Não tenho piadas, pare de encher!");
-    } 
-    
-    else if(session.message.text.toLowerCase().contains('charadinha') || session.message.text.toLowerCase().contains('imita o piasson')){
+    }
+
+    else if (session.message.text.toLowerCase().contains('charadinha') || session.message.text.toLowerCase().contains('imita o piasson')) {
 
         var url = "https://us-central1-kivson.cloudfunctions.net/charada-aleatoria"
-        
+
         request({
             url: url,
             json: true
         }, function (error, response, body) {
             var retorno = body.results;
             session.send(body.pergunta);
-            
-            setTimeout(function(resposta){
+
+            setTimeout(function (resposta) {
                 session.send(body.resposta);
-                setTimeout(function(resposta){
+                setTimeout(function (resposta) {
                     session.send('kkkkkkkkkkkkkkkkk');
                 }, 2000);
             }, 5000);
         })
-    } 
+    }
     else {
         session.send(lerolero());
     }
